4ecf47a309d3754d1c39763ce99f37d685657048
abhishek <abhisheksathala296@gmail.com>
2025-12-09 22:18:19 +0530

    add

--- src/controllers/v1/Comments/Create_comment.ts
+++ src/controllers/v1/Comments/Create_comment.ts
@@ -1,3 +1,10 @@
+// types
+import type { Response, Request } from 'express';
+import blogModel, { Iblog } from '@/models/blog';
+// custom modules
+import { logger } from '@/utils/winston';
+import commentModel from '@/models/comment';
+
 const Create_comment = () => {};
 
 export default Create_comment;
