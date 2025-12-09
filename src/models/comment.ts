3ce75ae9aed95c48a6d6d07a9b45b0ff9212bdeb
abhishek <abhisheksathala296@gmail.com>
2025-12-09 22:16:13 +0530

    add

--- src/models/comment.ts
+++ src/models/comment.ts
@@ -13,10 +13,12 @@ const CommentsSchema = new Schema<Icomment>(
     userId: {
       type: Schema.Types.ObjectId,
       ref: 'user',
+      required: true,
     },
     content: {
       type: String,
       required: [true, 'commet is requeried'],
+      maxLength: [1000, 'cant be long then 1k chars'],
     },
   },
   { timestamps: true },
