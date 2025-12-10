a7b41cb47d091b20a8b01934b774e68a168a0e96
abhishek <abhisheksathala296@gmail.com>
2025-12-10 12:00:44 +0530

    add

--- src/controllers/v1/Comments/Delete_comment.ts
+++ src/controllers/v1/Comments/Delete_comment.ts
@@ -1,3 +1,64 @@
-const Delete_comment = () => {};
+// types
+import type { Response, Request } from 'express';
+import blogModel, { Iblog } from '@/models/blog';
+import userModel from '@/models/user';
+import commentModel, { Icomment } from '@/models/comment';
+// custom modules
+import { logger } from '@/utils/winston';
+
+type commentData = Pick<Icomment, 'content'>;
+
+const Delete_comment = async (req: Request, res: Response): Promise<void> => {
+  const { blogId } = req.params;
+  const userId = req.userId;
+  const { content } = req.body as commentData;
+  if (!blogId || !userId) {
+    res.status(400).json({
+      code: 'BadRequest',
+      message: 'blogId and userId are required',
+      success: false,
+    });
+    return;
+  }
+  try {
+    const blog = await blogModel
+      .findById(blogId)
+      .select('_id commentsCounts')
+      .exec();
+    if (!blog) {
+      res.status(404).json({
+        code: 'NotFound',
+        message: 'Blog not found',
+        success: false,
+      });
+      return;
+    }
+    const user = await userModel.findById(userId).select('role').lean().exec();
+    if (!user) {
+      res.status(404).json({
+        code: 'NOT FOUND',
+        message:
+          'THE USER UR SEACRCHN DOS NOT EXIST BRO BE A GOOD BOY AND SEND CORRECT ID"s ',
+        success: false,
+      });
+      return;
+    }
+
+    logger.info('Blog comment added', { userId, blogId });
+    res.status(200).json({
+      code: 'Success',
+      message: 'comment added successfully',
+      success: true,
+      commentsCounts: blog.commentsCounts + 1,
+    });
+  } catch (error) {
+    logger.error('Error commet on blog  blog:', error);
+    res.status(500).json({
+      code: 'ServerError',
+      message: 'Internal server error',
+      success: false,
+    });
+  }
+};
 
 export default Delete_comment;
